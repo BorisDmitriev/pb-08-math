02d67c606ea2cbcb2cc1576807651ed1c8603e40
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:57:29 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,23 @@
+// 1a
+console.log(Math.min(-1,4));
+//1b
+console.log(Math.max(-1,4));
+
+
+//2a
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+//2b
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+//3
+console.log(Math.floor(Math.random() * 6) +1);
+
+
